41736bd20b2a6d8aef21c270f0b33b47b7f6df23
aomchanun <omsinjah@gmail.com>
2023-12-06 22:52:14 +0700

    submit lab06

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,77 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        // Create instances of your classes
+        human humanCharacter = new human();
+        undead undeadCharacter = new undead();
+        sword humanSword = new sword();
+        ward undeadWard = new ward();
+        sword undeadSword = new sword();
+        ward humanWard = new ward();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Set initial information and stats for characters
+        humanCharacter.characterinfomation("John", "Male");
+        humanCharacter.defaultstat();
+        undeadCharacter.characterinfomation("Zombie", "Unknown");
+        undeadCharacter.defaultstat();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Display initial stats
+        System.out.println("Initial Stats for Human:");
+        humanCharacter.getstat();
+        System.out.println("\nInitial Stats for Undead:");
+        undeadCharacter.getstat();
+        System.out.println();
+
+        // Equip accessories
+        humanCharacter.equip(humanSword);
+        undeadCharacter.equip(undeadWard);
+        humanCharacter.equip(humanSword);
+        undeadCharacter.equip(undeadWard);
+//        humanCharacter.equip(humanWard);
+//        undeadCharacter.equip(undeadSword);
+        System.out.println();
+
+        humanCharacter.levelup();
+        undeadCharacter.levelup();
+        System.out.println();
+
+        // Display stats after equipping accessories
+        System.out.println("\nStats for Human after equipping sword:");
+        humanCharacter.getstat();
+        System.out.println("\nStats for Undead after equipping ward:");
+        undeadCharacter.getstat();
+
+        // Unequip accessories
+        humanCharacter.unequip();
+        undeadCharacter.unequip();
+        humanCharacter.unequip();
+        undeadCharacter.unequip();
+
+        // Display stats after unequipping accessories
+        System.out.println("\nStats for Human after unequipping:");
+        humanCharacter.getstat();
+        System.out.println("\nStats for Undead after unequipping:");
+        undeadCharacter.getstat();
+        System.out.println();
+
+        humanCharacter.attack(undeadCharacter);
+        System.out.println("\nStats for Human after attack:");
+        humanCharacter.getstat();
+        System.out.println("\nStats for Undead after has been attack:");
+        undeadCharacter.getstat();
+        System.out.println();
+
+        undeadCharacter.attack(humanCharacter);
+        System.out.println("\nStats for Undead after attack:");
+        undeadCharacter.getstat();
+        System.out.println("\nStats for Human after has been attack:");
+        humanCharacter.getstat();
+        System.out.println();
+
+        humanCharacter.attack(undeadCharacter);
+        System.out.println("\nStats for Human after attack:");
+        humanCharacter.getstat();
+        System.out.println("\nStats for Undead after has been attack:");
+        undeadCharacter.getstat();
+
     }
 }
---
+++ src/accessories.java
@@ -0,0 +1,7 @@
+import java.lang.invoke.StringConcatFactory;
+
+public interface accessories {
+    public double getstat(human a);
+    public double getstat(undead a);
+
+}
